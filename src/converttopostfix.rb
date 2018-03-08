02b23aa0d89c49185c7a888fe9b099ff7e58000f
hoangle96 <lebahoang010296@gmail.com>
2018-03-08 10:56:49 -0500

    Update convert postfix

--- src/converttopostfix.rb
+++ src/converttopostfix.rb
@@ -1,70 +1,63 @@
-# class ConvertToPostFix
-#   postfix = Array.new
+def isOp(s)
+  ops = ["+", "-", "/", "*", "^", "%"]
+  return ops.include?(s) ? true : false
+end
 
-  def ConvertToPostFix(infix)
-    arrPf = Array.new
-    infix << ")"
+def precedence(op1, op2)
+  order = {
+      "^" => 3,
+      "*" => 2,
+      "/" => 2,
+      "%" => 2,
+      "+" => 1,
+      "-" => 1
+  }
+  return order[op1] < order[op2] ? true : false
+end
 
-    arrPf.push("(")
-    while !infix.empty? do
+def ConvertToPostFix(arr)
+  # regex to get the number
+  regex = /[0-9]/
 
-    end
+  # stack for operators, arr is the infix expression
+  stack = ["("]
+  arr << ")"
 
+  #postfix is the converted postfix expression
+  postfix = Array.new
 
-
-    # for i in 0...infix.length do
-    #   case
-    #     when !Integer(infix[i]).nan?
-    #       arrPf.push(infix[i])
-    #     when isOp(infix[i])
-    #       arrPf.each { |a|
-    #         if isOp(a)
-    #           if precedence(infix[i], a)
-    #             arrPf.push(a)
-    #           end
-    #         end
-    #       } # for each loop for the case that infix[i] is an element
-    #       arrPf.push(infix[i])
-    #     when infix[i] == "("
-    #       while
-    #       arrPf.push(infix[i])
-    #   end # end case
-    # end # end of for loop through infix
-  end # end of def ConvertToPostFix
-
-    def isOp(s)
-      ops = ["+","-","/","*","^","%"]
-      return ops.include?(s) ? true: false
-    end
-
-
-  def precedence(op1, op2)
-    order = {
-        "^" => 3,
-        "*" => 2,
-        "/" => 2,
-        "%" => 2,
-        "+" => 1,
-        "-" => 1
-    }
-    return order[op1] < order[op2] ? true : false
-  end # end precedence def
-
-
-  def isInt(s)
-    regex = /^[0-9]$/
+  # Loop through the infix expression
+  arr.each {|s|
     if s.match(regex)
-      return true
-    else
-      return false
+      postfix.push(s)
+    elsif s == "("
+      stack.push(s)
+    elsif isOp(s)
+      print(s + " is an op. ")
+      stack.reverse_each {|opsInStack|
+        if isOp(opsInStack) and !precedence(opsInStack, s)
+          print(opsInStack + "\n")
+          print(isOp(opsInStack))
+          postfix.push(stack.pop)
+        elsif !isOp(opsInStack)
+          break
+        end
+      }
+      stack.push(s)
+    elsif s == ")"
+      stack.reverse_each {|elementInStack|
+        if isOp(elementInStack) and stack.last != "("
+          postfix.push(stack.pop)
+        end
+      }
+      stack.pop
     end
+  }
+
+  # If the stack still have something, push it to the postfix
+  if (!stack.empty?)
+    postfix.push(stack.pop)
   end
 
-  def op
-
-  end
-
-# end #end of class
-
-
-print precedence("^", "*")
+  return postfix
+end
