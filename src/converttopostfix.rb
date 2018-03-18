ca96e70355803469f8bf1697ea6c4c376d797e95
hoangle96 <lebahoang010296@gmail.com>
2018-03-18 14:08:56 -0400

    Adding comments to the code

--- src/converttopostfix.rb
+++ src/converttopostfix.rb
@@ -4,6 +4,7 @@ module ConvertToPostfix
     return ops.include?(s) ? true : false
   end
 
+  # assign order of precedence to each operator then compare the orders of the two operators
   def precedence(op1, op2)
     order = {
         "^" => 3,
@@ -16,18 +17,23 @@ module ConvertToPostfix
     return order[op1] < order[op2] ? true : false
   end
 
-  def ConvertToPostFix(arr = [])
-    # regex to get the number
-    regex = /[0-9]/
 
-    # stack for operators, arr is the infix expression
+  def ConvertToPostFix(arr = []) # the default parameter is an empty array
+    regex = /[0-9]/  # regex matches number
+
+    # stack for operators, arr for infix expression
     stack = ["("]
     arr.push(")")
 
-    #postfix is the converted postfix expression
+    # postfix is the array for the converted postfix expression
     postfix = Array.new
 
-    # Loop through the infix expression
+    # Loop through the infix expression.
+    # If an element is numeric or a left parenthesis, it will be push to the postfix stack
+    # If an element is an operator, all the operators in the stack that have higher precedence will be pushed into the postfix expression,
+    # then the element will be pushed to the postfix expression.
+    # If an element is a right parenthesis, all the operators in the stack will be pushed into the postfix expression
+    # until a left parenthesis appears.
     arr.each {|s|
       if s.match(regex)
         postfix.push(s)
--- src/eval.rb
+++ src/eval.rb
@@ -1,5 +1,5 @@
  module Eval
-  #https://stackoverflow.com/questions/19733737/how-do-i-convert-a-math-operator-character-to-perform-its-operation
+  # https://stackoverflow.com/questions/19733737/how-do-i-convert-a-math-operator-character-to-perform-its-operation
   def calc(x, op, y)
     if op == "^"
       op = "**"
@@ -7,11 +7,16 @@
     return(x.to_i.send(op, y.to_i))
   end
 
+  # evalE takes the postfix stack (array), iterates through each element of the stack.
+  # If the element is a number, push it in the postfix stack.
+  # If the element is an operator, pop the 2 preceding numbers and do the calculation,
+  # then push the result to the stack.
   def evalE(pft)
     pfEnd = pft.length
     regex = /[0-9]/
     arrT = Array.new
 
+    # Iterate through each element of
     for i in 0...pft.length do
       if pft[i].match(regex)
         arrT.push(pft[i]) # this is where we load up the stack
